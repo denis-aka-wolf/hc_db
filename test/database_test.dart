e180bc904b38059b08b2a4e23e583dd6eda8f296
denis-aka-wolf <denis.aka.wolf@gmail.com>
2025-12-09 21:47:57 +0300

    Добавил тесты на проверку создаваемых файлов

--- test/database_test.dart
+++ test/database_test.dart
@@ -6,7 +6,7 @@ import 'dart:io';
 final List<String> createdDatabases = [];
 
 void main() {
-  group('Create Database', () {
+ group('Database Creation Tests', () {
     setUp(() {
       // Создаем директорию db перед тестами, если она не существует
       Directory('./db').createSync(recursive: true);
@@ -66,18 +66,18 @@ void main() {
       // Тестируем создание базы данных с типом таблицы balance
       final db = await Database.createDatabase(
         directoryPath: './db',
-        databaseName: 'test_db_balance',
+        databaseName: 'test_db_balance_unique',
         tableType: TableType.balance,
         measurements: ['measurement1'],
         resources: ['resource1'],
       );
       
       // Добавляем созданную базу данных в список для последующего удаления
-      createdDatabases.add('test_db_balance');
+      createdDatabases.add('test_db_balance_unique');
       
       // Проверяем, что свойства установлены правильно
       expect(db.directoryPath, './db');
-      expect(db.databaseName, 'test_db_balance');
+      expect(db.databaseName, 'test_db_balance_unique');
       expect(db.tableType, TableType.balance);
       expect(db.measurements, ['measurement1']);
       expect(db.resources, ['resource1']);
@@ -87,18 +87,18 @@ void main() {
       // Тестируем создание базы данных с типом таблицы turnover
       final db = await Database.createDatabase(
         directoryPath: './db',
-        databaseName: 'test_db_turnover',
+        databaseName: 'test_db_turnover_unique',
         tableType: TableType.turnover,
         measurements: ['measurement1'],
         resources: ['resource1'],
       );
       
       // Добавляем созданную базу данных в список для последующего удаления
-      createdDatabases.add('test_db_turnover');
+      createdDatabases.add('test_db_turnover_unique');
       
       // Проверяем, что свойства установлены правильно
       expect(db.directoryPath, './db');
-      expect(db.databaseName, 'test_db_turnover');
+      expect(db.databaseName, 'test_db_turnover_unique');
       expect(db.tableType, TableType.turnover);
       expect(db.measurements, ['measurement1']);
       expect(db.resources, ['resource1']);
@@ -108,21 +108,172 @@ void main() {
       // Тестируем создание базы данных с типом таблицы universal
       final db = await Database.createDatabase(
         directoryPath: './db',
-        databaseName: 'test_db_universal',
+        databaseName: 'test_db_universal_unique',
         tableType: TableType.universal,
         measurements: ['measurement1'],
         resources: ['resource1'],
       );
       
       // Добавляем созданную базу данных в список для последующего удаления
-      createdDatabases.add('test_db_universal');
+      createdDatabases.add('test_db_universal_unique');
       
       // Проверяем, что свойства установлены правильно
       expect(db.directoryPath, './db');
-      expect(db.databaseName, 'test_db_universal');
+      expect(db.databaseName, 'test_db_universal_unique');
       expect(db.tableType, TableType.universal);
       expect(db.measurements, ['measurement1']);
       expect(db.resources, ['resource1']);
+    });
+  });
+  
+  group('Database Files Creation Tests', () {
+    setUp(() {
+      // Создаем директорию db перед тестами, если она не существует
+      Directory('./db').createSync(recursive: true);
+    });
+    
+    tearDown(() {
+      // Удаляем только созданные тестами базы данных
+      for (final dbName in createdDatabases) {
+        final dbDir = Directory('./db/$dbName');
+        if (dbDir.existsSync()) {
+          dbDir.deleteSync(recursive: true);
+        }
+      }
+      // Очищаем список созданных баз данных
+      createdDatabases.clear();
+    });
+    
+    test('Database files creation for balance type', () async {
+      // Тестируем создание файлов базы данных с типом balance
+      await Database.createDatabase(
+        directoryPath: './db',
+        databaseName: 'test_db_files_balance',
+        tableType: TableType.balance,
+        measurements: ['measurement1', 'measurement2'],
+        resources: ['resource1', 'resource2'],
+      );
+      
+      // Добавляем созданную базу данных в список для последующего удаления
+      createdDatabases.add('test_db_files_balance');
+      
+      // Проверяем, что база данных создана с правильными файлами
+      final dbDir = Directory('./db/test_db_files_balance');
+      expect(await dbDir.exists(), true);
+      
+      // Для типа balance должны быть созданы файлы movements и aggregations
+      final movementsFile = File('./db/test_db_files_balance/test_db_files_balance.movements');
+      final aggregationsFile = File('./db/test_db_files_balance/test_db_files_balance.aggregations');
+      final turnoversFile = File('./db/test_db_files_balance/test_db_files_balance.turnovers');
+      
+      expect(await movementsFile.exists(), true);
+      expect(await aggregationsFile.exists(), true);
+      expect(await turnoversFile.exists(), false); // turnovers не должен создаваться для типа balance
+    });
+    
+    test('Database files creation for turnover type', () async {
+      // Тестируем создание файлов базы данных с типом turnover
+      await Database.createDatabase(
+        directoryPath: './db',
+        databaseName: 'test_db_files_turnover',
+        tableType: TableType.turnover,
+        measurements: ['measurement1', 'measurement2'],
+        resources: ['resource1', 'resource2'],
+      );
+      
+      // Добавляем созданную базу данных в список для последующего удаления
+      createdDatabases.add('test_db_files_turnover');
+      
+      // Проверяем, что база данных создана с правильными файлами
+      final dbDir = Directory('./db/test_db_files_turnover');
+      expect(await dbDir.exists(), true);
+      
+      // Для типа turnover должны быть созданы файлы movements и turnovers
+      final movementsFile = File('./db/test_db_files_turnover/test_db_files_turnover.movements');
+      final turnoversFile = File('./db/test_db_files_turnover/test_db_files_turnover.turnovers');
+      final aggregationsFile = File('./db/test_db_files_turnover/test_db_files_turnover.aggregations');
+      
+      expect(await movementsFile.exists(), true);
+      expect(await turnoversFile.exists(), true);
+      expect(await aggregationsFile.exists(), false); // aggregations не должен создаваться для типа turnover
+    });
+    
+    test('Database files creation for universal type', () async {
+      // Тестируем создание файлов базы данных с типом universal
+      await Database.createDatabase(
+        directoryPath: './db',
+        databaseName: 'test_db_files_universal',
+        tableType: TableType.universal,
+        measurements: ['measurement1', 'measurement2'],
+        resources: ['resource1', 'resource2'],
+      );
+      
+      // Добавляем созданную базу данных в список для последующего удаления
+      createdDatabases.add('test_db_files_universal');
+      
+      // Проверяем, что база данных создана с правильными файлами
+      final dbDir = Directory('./db/test_db_files_universal');
+      expect(await dbDir.exists(), true);
+      
+      // Для типа universal должны быть созданы файлы movements, aggregations и turnovers
+      final movementsFile = File('./db/test_db_files_universal/test_db_files_universal.movements');
+      final aggregationsFile = File('./db/test_db_files_universal/test_db_files_universal.aggregations');
+      final turnoversFile = File('./db/test_db_files_universal/test_db_files_universal.turnovers');
+      
+      expect(await movementsFile.exists(), true);
+      expect(await aggregationsFile.exists(), true);
+      expect(await turnoversFile.exists(), true);
+    });
+    
+    test('Database config file creation', () async {
+      // Тестируем создание файла конфигурации
+      await Database.createDatabase(
+        directoryPath: './db',
+        databaseName: 'test_db_config',
+        tableType: TableType.universal,
+        measurements: ['measurement1'],
+        resources: ['resource1'],
+      );
+      
+      // Добавляем созданную базу данных в список для последующего удаления
+      createdDatabases.add('test_db_config');
+      
+      // Проверяем, что файл конфигурации создан
+      final configFile = File('./db/test_db_config/test_db_config.config');
+      expect(await configFile.exists(), true);
+      
+      // Проверяем содержимое файла конфигурации
+      final configContent = await configFile.readAsString();
+      expect(configContent.contains('databaseName'), true);
+      expect(configContent.contains('tableType'), true);
+      expect(configContent.contains('measurements'), true);
+      expect(configContent.contains('resources'), true);
+    });
+    
+    test('Database creation with same name should throw error', () async {
+      // Создаем базу данных
+      await Database.createDatabase(
+        directoryPath: './db',
+        databaseName: 'test_db_duplicate',
+        tableType: TableType.balance,
+        measurements: ['measurement1'],
+        resources: ['resource1'],
+      );
+      
+      // Добавляем созданную базу данных в список для последующего удаления
+      createdDatabases.add('test_db_duplicate');
+      
+      // Пробуем создать базу данных с тем же именем - должно вызвать ошибку
+      expect(
+        () async => await Database.createDatabase(
+          directoryPath: './db',
+          databaseName: 'test_db_duplicate',
+          tableType: TableType.balance,
+          measurements: ['measurement1'],
+          resources: ['resource1'],
+        ),
+        throwsA(isA<StateError>()),
+      );
     });
   });
 }
